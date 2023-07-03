6bfdecf28097c00c5a6f069c31e67849f03cb51c
Wahyu Adhe Triwibowo <adhedewa1996@gmail.com>
2023-07-03 15:17:40 +0700

    change version

--- lib/src/widget/appbars.dart
+++ lib/src/widget/appbars.dart
@@ -5,7 +5,47 @@ import '../design_token/index.dart';
 import 'images.dart';
 
 mixin Appbars {
-  static Widget normal({
+  static Widget simple({
+    Color backgroundColor = Colors.grey,
+    BoxFit boxfitImage = BoxFit.fitWidth,
+    required Widget child,
+    required IconData rightIcon,
+    required Color rightIconColor,
+    required IconData leftIcon,
+    required Color leftIconColor,
+  }) {
+    return AspectRatio(
+      aspectRatio: 6 / 1,
+      child: Container(
+        padding: const EdgeInsets.all(CapybaraPadding.small),
+        color: backgroundColor,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(CapybaraPadding.small),
+              child: Icon(leftIcon, color: leftIconColor),
+            ),
+            child,
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              padding: const EdgeInsets.all(CapybaraPadding.small),
+              child: Icon(rightIcon, color: rightIconColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  static Widget withImageAndIcon({
     Color backgroundColor = Colors.grey,
     BoxFit boxfitImage = BoxFit.fitWidth,
     required String urlImage,
